4434e0808e223db35f870b59df46f2c4191ed788
aelmouja <ayoub_moujahid@hotmail.com>
2025-04-29 14:28:11 +0300

    404

--- src/components/pages/error.jsx
+++ src/components/pages/error.jsx
@@ -1,11 +1,16 @@
+import '../css/error.css';
+import { useNavigate } from "react-router-dom";
+
 const Error404 = () => {
-    return (
-        <div className='error-page'>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        <a href="/">Go back to Home</a>
-        </div>
-    );
-}
+  const navigate = useNavigate();
+
+  return (
+    <div className="error-page">
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+    </div>
+  );
+};
 
 export default Error404;
